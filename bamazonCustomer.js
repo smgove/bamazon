93f2a1ed03899f378a08c050cb9c7a3a98fb0139
Shane Gove <shanemgove@gmail.com>
2019-04-28 10:31:34 -0500

    Final

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,11 +10,10 @@ var connection = mysql.createConnection({
   // Your username
   user: "root",
   // Your password
-  password: "Ahamay125!",
+  password: "",
   database: "bamazondb"
 });
-//the below uses cli table to format the output - you push the response 
-//items into an array after iterating through the response
+
 
 var showAllItems = function () {
   connection.query("SELECT * FROM products", function (err, res) {
@@ -30,8 +29,7 @@ var showAllItems = function () {
     customerChoice()
   });
 };
-//need to get item to add into inquirer question
-//here we request that the user enter an item id and we pass that to our SQL
+
 var customerChoice = function () {
   inquirer.prompt({
     name: "myProduct",
@@ -44,11 +42,11 @@ var customerChoice = function () {
       function (err, res) {
         if (err) throw err;
         if (res.length === 0) {
-          //1st check to see if we got a value back
+     
           console.log("Please enter a valid item");
           customerChoice();
         }
-        //now ask for the quantity
+       
         else {
           inquirer.prompt({
 
@@ -57,23 +55,22 @@ var customerChoice = function () {
             message: "How many would you like to buy?"
           })
             .then(function (antwo) {
-              //wait for a response
               var quantity = antwo.quantity;
               var updatedQuantity = res[0].stock_quantity - quantity;
               var totalOrderPrice = quantity * res[0].price;
-              //check to see if customer has ordered more than avail
+            
               if (quantity > res[0].stock_quantity) {
                 console.log("We don't have sufficient stock. Please enter a lower quantity.")
                 customerChoice();
               }
-              //if there is enough then reduce the inventory by amount ordered - this is currently broken
+              
               else {
                 connection.query("UPDATE products SET stock_quantity  =? where item_id =?", [updatedQuantity, choice],
                   function (err, res) {
                     if (err) throw err;
                   })                
                 console.log("We've fufilled your order.  Your cost was $" + totalOrderPrice);
-                process.exit(-1);; //exit once order complete
+                process.exit(-1);; 
               }
             })
         }
